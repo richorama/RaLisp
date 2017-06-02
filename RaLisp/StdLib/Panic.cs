3c1676a8d033acbdfc93151dd4cf3974c3893fc9
Richard Astbury <richard.astbury@gmail.com>
2017-06-02 12:15:53 +0100

    add panic

---
+++ RaLisp/StdLib/Panic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RaLisp.StdLib
+{
+    public class Panic : IFunction
+    {
+        public string Name
+        {
+            get
+            {
+                return "panic";
+            }
+        }
+
+        public object Execute(IDictionary<string, object> context, params object[] parameters)
+        {
+            var exitCode = -1;
+            if (parameters.Any())
+            {
+                var value = parameters[0].Evaluate(context);
+                int.TryParse(value.ToString(), out exitCode);
+            }
+            System.Environment.Exit(exitCode);
+            return null;
+        }
+    }
+}
--- RaLispTests/TestStdLib.cs
+++ RaLispTests/TestStdLib.cs
@@ -309,7 +309,7 @@ namespace RaLispTests
 
         }
 
-
+     
     }
 
 
